eccaa65b200f3d60d2114b8413fd277767416e9b
unknown <aritrodam773@gmail.com>
2025-04-01 20:36:59 +0530

    Error handling for list display implemented

--- app/src/main/java/com/example/dampokedex/pokemonlist/PokemonListScreen.kt
+++ app/src/main/java/com/example/dampokedex/pokemonlist/PokemonListScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -211,10 +212,24 @@ fun PokemonList(
             pokemonList.count() / 2 + 1
         }
         items(itemCount) {
-            if (it >= itemCount - 1 && !endReached) {
+            if (it >= itemCount - 1 && !endReached && !isLoading) {
                 viewModel.loadPokemonPaginated()
             }
             PokedexRow(rowIndex = it, entries = pokemonList, navController = navController)
+        }
+    }
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+        ) {
+        if (isLoading) {
+            CircularProgressIndicator(color = MaterialTheme.colorScheme.primary)
+        }
+        else if (loadError.isNotEmpty()) {
+            RetryScetion(loadError) {
+                viewModel.loadPokemonPaginated()
+            }
         }
     }
 }
@@ -244,5 +259,22 @@ fun PokedexRow(
             }
         }
         Spacer(modifier = Modifier.height(16.dp))
+    }
+}
+
+@Composable
+fun RetryScetion(
+    error: String,
+    onRetry: () -> Unit
+) {
+    Column {
+        Text(error, color = Color.Red, fontSize = 18.sp)
+        Spacer(modifier = Modifier
+            .height(8.dp))
+        Button(onClick = {onRetry()},
+            modifier = Modifier
+                .align(alignment = Alignment.CenterHorizontally)) {
+            Text(text = "Retry")
+        }
     }
 }
